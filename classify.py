f5aae771c947af1672ebc2c6f0b54e1ede91dea4
dobribobri <dobrix95@gmail.com>
2023-04-21 18:13:24 +0300

    210423

--- classify.py
+++ classify.py
@@ -1,10 +1,15 @@
-
 # -*- coding: utf-8 -*-
+import sys
 import itertools
+from collections import defaultdict
 import numpy as np
 import networkx as nx
 import kuramoto
-from sklearn import datasets
+from sklearn import datasets, preprocessing, model_selection, metrics
+from scipy.special import softmax
+from multiprocessing import Manager, Process, Pool
+import tqdm
+from matplotlib import pyplot as plt
 
 
 seed = 42
@@ -39,21 +44,12 @@ class Kuramoto(kuramoto.Kuramoto):
         return meanfreq
 
 
-class TimeDomain:
-    def __init__(self, dt: float = 0.1, t_step: float = 10, T: float = 150000):
-        self.dt = dt
-        self.t_step = t_step
-        self.T = T
-
-    def get_parameters(self):
-        return self.dt, self.t_step, self.T
-
-
 class OscillatoryNeuralNetwork:
     def __init__(self, input_layer_size: int, output_layer_size: int,
                  hidden_layer_sizes=(4,),
                  oriented: bool = False,
-                 coupling_global: float = 1.,):
+                 coupling_global: float = 10.,
+                 dt: float = 0.1, t_step: float = 10, T: float = 100):
         if hidden_layer_sizes is None:
             hidden_layer_sizes = []
         self.n_nodes = input_layer_size + int(np.sum(hidden_layer_sizes)) + output_layer_size
@@ -62,37 +58,11 @@ class OscillatoryNeuralNetwork:
         self.coupling_global = coupling_global
         self.adj_mat = nx.to_numpy_array(self.G) * self.coupling_global
         self.adj_mat *= np.random.random(self.adj_mat.shape)
+        self.adj_mat += 0.1
         self.input_layer_size, self.output_layer_size = input_layer_size, output_layer_size
         self.hidden_layer_sizes = hidden_layer_sizes
-
-    def run(self, time_domain: TimeDomain, angles_vec_init: np.ndarray, nat_freqs_init: np.ndarray,):
-        dt, t_step, T = time_domain.get_parameters()
-        time_ = np.array([])
-        angles_ = np.array([[]] * self.n_nodes)
-        frequencies_ = np.array([[]] * self.n_nodes)
-        phase_coherence_ = np.array([])
-        angles_vec = angles_vec_init
-        natfreqs = nat_freqs_init
-        for t_curr in np.arange(0., T, t_step):
-            model = Kuramoto(coupling=1, dt=dt, T=t_step, n_nodes=self.n_nodes, natfreqs=natfreqs,)
-            activity = model.run(adj_mat=self.adj_mat, angles_vec=angles_vec)
-            frequencies = model.frequencies(act_mat=activity, adj_mat=self.adj_mat)
-            phase_coherence = np.asarray([Kuramoto.phase_coherence(vec) for vec in activity.T])
-            time_ = np.hstack((time_, np.arange(t_curr, t_curr + t_step, dt)))
-            angles_ = np.hstack((angles_, np.sin(activity)))
-            frequencies_ = np.hstack((frequencies_, frequencies))
-            phase_coherence_ = np.hstack((phase_coherence_, phase_coherence))
-            if np.all(np.isclose(phase_coherence, 1.)):
-                break
-            angles_vec = activity.T[-1]
-            natfreqs = frequencies.T[-1]
-        return time_, angles_, frequencies_, phase_coherence_
-
-    def forward_frequencies(self, x: np.ndarray, time_domain: TimeDomain, angles_vec_init: np.ndarray) -> np.ndarray:
-        assert self.input_layer_size == len(x), 'wrong shape for input data'
-        _, _, frequencies, _ = self.run(time_domain, angles_vec_init, nat_freqs_init=x)
-        return frequencies.T[-1][-self.output_layer_size]
-
+        self.dt, self.t_step, self.T = dt, t_step, T
+        self.genes = self.genes = list(itertools.product(range(self.n_nodes), range(self.n_nodes)))
 
     @staticmethod
     def __multilayered_graph(oriented=False, *subset_sizes) -> nx.Graph:
@@ -108,10 +78,183 @@ class OscillatoryNeuralNetwork:
             G.add_edges_from(itertools.product(layer1, layer2))
         return G
 
+    def run(self, _angles_vec_init: np.ndarray, _nat_freqs_init: np.ndarray,):
+        time_ = np.array([])
+        angles_ = np.array([[]] * self.n_nodes)
+        frequencies_ = np.array([[]] * self.n_nodes)
+        phase_coherence_ = np.array([])
+        angles_vec = _angles_vec_init
+        natfreqs = _nat_freqs_init
+        for t_curr in np.arange(0., self.T, self.t_step):
+            model = Kuramoto(coupling=1, dt=self.dt, T=self.t_step, n_nodes=self.n_nodes, natfreqs=natfreqs,)
+            activity = model.run(adj_mat=self.adj_mat, angles_vec=angles_vec)
+            frequencies = model.frequencies(act_mat=activity, adj_mat=self.adj_mat)
+            phase_coherence = np.asarray([Kuramoto.phase_coherence(vec) for vec in activity.T])
+            time_ = np.hstack((time_, np.arange(t_curr, t_curr + self.t_step, self.dt)))
+            angles_ = np.hstack((angles_, np.sin(activity)))
+            frequencies_ = np.hstack((frequencies_, frequencies))
+            phase_coherence_ = np.hstack((phase_coherence_, phase_coherence))
+            if np.all(np.isclose(phase_coherence, 1.)):
+                break
+            angles_vec = activity.T[-1]
+            natfreqs = frequencies.T[-1]
+        return time_, angles_, frequencies_, phase_coherence_
+
+    def forward(self, _x: np.ndarray, _angles_vec_init: np.ndarray, _apply=lambda x: x) -> np.ndarray:
+        assert self.input_layer_size == len(_x), 'wrong shape for input data'
+        _, _, frequencies, _ = self.run(_angles_vec_init=_angles_vec_init,
+                                        _nat_freqs_init=np.concatenate((_x, np.zeros(self.n_nodes - len(_x)))))
+        out = frequencies.T[-1][-self.output_layer_size:]
+        return _apply(out)
+
+    def forward_multiple(self, _X: np.ndarray, _angles_vec_init: np.ndarray, _apply=lambda x: x) -> np.ndarray:
+        out = np.asarray([self.forward(_x, _angles_vec_init) for _x in _X])
+        return _apply(out)
+
+    def error_on_batch(self, _X: np.ndarray, _y_true: np.ndarray, _angles_vec_init: np.ndarray) -> float:
+        assert _X.shape[0] == _y_true.shape[0], 'X and y_true must have the same number of samples'
+        assert self.output_layer_size == _y_true.shape[1], 'wrong shape (y_true)'
+        out = self.forward_multiple(_X, _angles_vec_init, _apply=lambda b: softmax(b, axis=1))
+        return np.linalg.norm(_y_true - out)
+
+    def crossover(self, obj: 'OscillatoryNeuralNetwork', percentage: float = 0.5) -> None:
+        sh, sw = self.adj_mat.shape
+        oh, ow = obj.adj_mat.shape
+        assert sh == sw == oh == ow == self.n_nodes, 'wrong shape'
+        transfer = [obj.genes[_i] for _i in np.random.choice(range(len(obj.genes)), int(len(obj.genes) * percentage))]
+        for _i, _j in transfer:
+            self.adj_mat[_i][_j] = obj.adj_mat[_i][_j]
+
+    def mutation(self, n: int):
+        mutable = [self.genes[_i] for _i in np.random.choice(range(len(self.genes)), n)]
+        for _i, _j in mutable:
+            self.adj_mat[_i][_j] = np.random.random() * self.coupling_global
 
 
 if __name__ == '__main__':
-    iris = datasets.load_iris()
-    X = iris.data
-    Y = iris.target
-    print(X.shape, Y.shape)
+    print('Hello!')
+    X, y = datasets.load_iris(return_X_y=True)
+    label_encoder = preprocessing.LabelEncoder()
+    y = label_encoder.fit_transform(y)
+    y_categorical = np.zeros((len(y), len(label_encoder.classes_)))
+    for i, j in enumerate(y):
+        y_categorical[i][j] = 1
+    y = y_categorical
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
+
+    n_workers = 8
+    n_individuals = 100
+    n_best = 10
+    n_new = 10
+    n_genes_mutable = 10
+    n_epochs = sys.maxsize
+
+    initial_population = []
+    for i in range(n_individuals):
+        initial_population.append(
+            OscillatoryNeuralNetwork(input_layer_size=4, output_layer_size=3, hidden_layer_sizes=None)
+        )
+    population = initial_population
+
+    angles_vec_init = np.zeros(4 + 3)
+
+    plt.ion()
+    fig, ax = plt.subplots(nrows=2, figsize=(10, 7))
+    # ax[0].set_title('loss')
+    # ax[0].set_xlabel('epoch')
+    ax[0].set_ylabel('value')
+    # ax[1].set_title('f1-score')
+    ax[1].set_xlabel('epoch')
+    ax[1].set_ylabel(r'$\%$')
+
+    losses = defaultdict(list)
+    f1_train_list, f1_test_list = [], []
+    colors = ['crimson', 'forestgreen', 'black']
+
+    def process(args) -> tuple[float, float]:
+        _i, _individual = args
+        return _i, _individual.error_on_batch(X_train, y_train, angles_vec_init)
+
+
+    for epoch in range(n_epochs):
+        print('Epoch #{}'.format(epoch))
+
+        scores = []
+        with Pool(processes=n_workers) as pool:
+            for score in tqdm.tqdm(pool.imap_unordered(process, enumerate(population)),
+                                   total=len(population)):
+                scores.append(score)
+            # scores = pool.imap_unordered(process, enumerate(population))
+
+        errors = sorted(scores, key=lambda item: item[1])
+        values = [val for _, val in errors]
+
+        s = '\r{:.2f}%\t-\tepoch: {}\t-\tloss: {:.2f} (1 best),\t{:.2f} ({} best),\t{:.2f} (total)\t-\t'.format(
+            (epoch + 1.) / n_epochs * 100,
+            epoch + 1,
+            np.mean(values[:1]),
+            np.mean(values[:n_best]),
+            n_best,
+            np.mean(values)
+        )
+        losses['best'].append(np.mean(values[:1]))
+        losses['{} best'.format(n_best)].append(np.mean(values[:n_best]))
+        losses['total'].append(np.mean(values))
+        epochs = np.array(list(range(0, epoch + 1))) + 1
+        for i, key in enumerate(losses.keys()):
+            ax[0].plot(epochs, losses[key], label='loss: {}'.format(key), color=colors[i])
+        if not epoch:
+            ax[0].legend(loc='best', frameon=False)
+
+        best, other = [i for i, _ in errors[:n_best]], [i for i, _ in errors[n_best:]]
+        p = np.asarray(population, dtype=object)
+        best_individuals, other_individuals = p[best], p[other]
+
+        best_out_train = best_individuals[0].forward_multiple(X_train, angles_vec_init,
+                                                              _apply=lambda _y: softmax(_y, axis=1))
+        best_out_test = best_individuals[0].forward_multiple(X_test, angles_vec_init,
+                                                             _apply=lambda _y: softmax(_y, axis=1))
+        # print(y_train)
+        # print(best_out_train)
+        f1_train = metrics.f1_score(y_train, best_out_train, average='weighted')
+        f1_test = metrics.f1_score(y_test, best_out_test, average='weighted')
+        s += 'f1: {:.2f} (train),\t{:.2f} (test)'.format(
+            f1_train, f1_test
+        )
+        print(s, end='', flush=True)
+
+        f1_train_list.append(f1_train * 100.)
+        f1_test_list.append(f1_test * 100.)
+        ax[1].plot(epochs, f1_train_list, label='f1-score: train', color=colors[0])
+        ax[1].plot(epochs, f1_test_list, label='f1-score: test', color=colors[1])
+        if not epoch:
+            ax[1].legend(loc='best', frameon=False)
+
+        plt.savefig('evolution.png', dpi=300)
+        plt.show(block=False)
+        fig.canvas.flush_events()
+
+        np.random.shuffle(best_individuals)
+        np.random.shuffle(other_individuals)
+        # Новая кровь
+        for i in range(len(other_individuals) - n_new, len(other_individuals)):
+            other_individuals[i] = OscillatoryNeuralNetwork(input_layer_size=4, output_layer_size=3,
+                                                            hidden_layer_sizes=None)
+        # Скрещивание
+        print('{} | crossover'.format(s), end='', flush=True)
+        for individual in other_individuals[-n_new:]:
+            best_parent = np.random.choice(best_individuals)
+            individual.crossover(best_parent, percentage=0.5)
+
+        # Мутация
+        print('{} | mutation'.format(s), end='', flush=True)
+        for individual in other_individuals[-n_new:]:
+            individual.mutation(n=n_genes_mutable)
+        print(s, end='', flush=True)
+        new_population = best_individuals.tolist() + other_individuals.tolist()
+        np.random.shuffle(new_population)
+        population = new_population
+
+    plt.show()
+    plt.ioff()
+    plt.close()
--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import kuramoto
 import numpy as np
 import networkx as nx
 from networkx.drawing.nx_pydot import graphviz_layout
+import itertools
 from scipy import stats, signal
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("TkAgg")
 
 
-dt, t_step, T = 0.1, 10, 150000
+dt, t_step, T = 0.1, 10, 15000
 
-seed = 42
+seed = 28
 np.random.seed(seed)
 
 
@@ -77,16 +78,30 @@ def run(_graph: np.ndarray, _angles_vec_init: np.ndarray, _nat_freqs_init: np.nd
     return activity_, frequencies_
 
 
+def multilayered_graph(oriented=False, *subset_sizes) -> nx.Graph:
+    extents = nx.utils.pairwise(itertools.accumulate((0,) + subset_sizes))
+    layers = [range(start, end) for start, end in extents]
+    if oriented:
+        G = nx.DiGraph()
+    else:
+        G = nx.Graph()
+    for _i, layer in enumerate(layers):
+        G.add_nodes_from(layer, layer=_i)
+    for layer1, layer2 in nx.utils.pairwise(layers):
+        G.add_edges_from(itertools.product(layer1, layer2))
+    return G
+
+
 if __name__ == '__main__':
-    n_neurons = 10
+    n_neurons = 7
 
     # graph_nx = nx.erdos_renyi_graph(n=n_neurons, p=1)     # полносвязный
 
     # graph_nx = nx.circulant_graph(n_neurons, [1])   # кольцо
 
-    graph_nx = nx.Graph()   # звезда
-    graph_nx.add_nodes_from(range(n_neurons))
-    graph_nx.add_edges_from(zip([0] * (n_neurons - 1), range(1, n_neurons)))
+    # graph_nx = nx.Graph()   # звезда
+    # graph_nx.add_nodes_from(range(n_neurons))
+    # graph_nx.add_edges_from(zip([0] * (n_neurons - 1), range(1, n_neurons)))
 
     # graph_nx = nx.Graph()  # линейка
     # graph_nx.add_nodes_from(range(n_neurons))
@@ -94,25 +109,40 @@ if __name__ == '__main__':
 
     # graph_nx = nx.erdos_renyi_graph(n=n_neurons, p=0.5)     # случайный
 
+    graph_nx = multilayered_graph(True, *[4, 3])
+
+    # n_neurons = 8
+    # extents = nx.utils.pairwise(itertools.accumulate((0,) + (4, 3, 1)))
+    # layers = [range(start, end) for start, end in extents]
+    # G = nx.DiGraph()
+    # for _i, layer in enumerate(layers):
+    #     G.add_nodes_from(layer, layer=_i)
+    # for layer1, layer2 in nx.utils.pairwise(layers):
+    #     G.add_edges_from(itertools.product(layer1, layer2))
+    # G.add_edges_from(itertools.product(layers[2], layers[1]))
+    # graph_nx = G
+    #
     pos = graphviz_layout(graph_nx, prog='dot')
     nx.draw(graph_nx, pos, node_color='lightblue', edge_color='#909090', node_size=200, with_labels=True)
     plt.show()
-
-    graph = nx.to_numpy_array(graph_nx) * np.random.rand(n_neurons, n_neurons) * 1.
-    # graph = nx.to_numpy_array(graph_nx)
 
     angles_vec = 2 * np.pi * np.random.random(size=n_neurons) - np.pi
     angles_vec *= 0.
 
     # natfreqs = np.random.normal(loc=1., scale=0.1, size=n_neurons)
-    natfreqs = np.random.normal(size=n_neurons)
-    natfreqs *= 0.
+    # natfreqs = np.random.random(size=n_neurons)
+    # natfreqs *= 0.
+    natfreqs = [5.1, 3.5, 1.4, 0.2, 0, 0, 0]
+
+    graph = nx.to_numpy_array(graph_nx) * np.random.rand(n_neurons, n_neurons) * 10 + 0.4
+    # graph = nx.to_numpy_array(graph_nx) * np.random.rand(n_neurons, n_neurons) + 0.001
+    # graph = nx.to_numpy_array(graph_nx)
 
     plt.ion()
     fig, axes = plt.subplots(figsize=(10, 7), ncols=1, nrows=3)
-    axes[0].set_ylabel(r'$\sin(\theta)$', rotation=0)
+    axes[0].set_ylabel(r'$\sin(\theta_i)$', rotation=0)
     axes[1].set_ylabel('Coherence')
-    axes[2].set_ylabel(r'$\omega$', rotation=0)
+    axes[2].set_ylabel(r'$\omega_i$', rotation=0)
     plt.show(block=False)
 
     x_time = np.array([])
@@ -121,9 +151,8 @@ if __name__ == '__main__':
     x_time2 = [0]
     y_mean_freqs = [natfreqs]
     y_last_freqs = [natfreqs]
-    # x_time3 = []
-    # y_partial_stat = []
 
+    f_norms = []
     for j, t_curr in enumerate(np.arange(0., T + t_step, t_step)):
         activity, frequencies = run(_graph=graph, _angles_vec_init=angles_vec, _nat_freqs_init=natfreqs)
 
@@ -133,8 +162,8 @@ if __name__ == '__main__':
         freqs_vec = frequencies.T[-1]
         mean_freqs = np.sum(frequencies * dt, axis=1) / t_step
 
-        # natfreqs[:] = mean_freqs[:]
-        natfreqs[:] = freqs_vec[:]
+        # natfreqs = mean_freqs
+        natfreqs = freqs_vec
 
         x_time = np.hstack((x_time, np.arange(t_curr, t_curr + t_step, dt)))
         y_angles = np.hstack((y_angles, np.sin(activity)))
@@ -147,41 +176,32 @@ if __name__ == '__main__':
         for i in range(3):
             axes[i].clear()
 
-        axes[0].set_ylabel(r'$\sin(\theta)$', rotation=0)
+        axes[0].set_ylabel(r'$\sin(\theta_i)$', rotation=0)
         axes[1].set_ylabel('Coherence')
         # axes[2].set_ylabel(r'$\overline{\omega} = $' + '{:.3f}'.format(np.mean(mean_freqs)), rotation=0)
-        axes[2].set_ylabel(r'$\omega$', rotation=0)
+        axes[2].set_ylabel(r'$\omega_i$', rotation=0)
 
         N = int(t_step/dt)
 
         axes[0].plot(x_time[-10*N:], y_angles.T[-10*N:])
         axes[1].plot(x_time[-100*N:], y_phase_coherence[-100*N:], color='forestgreen')
-        axes[2].plot(x_time2[-100:], y_mean_freqs[-100:])
-        # axes[2].plot(x_time2[-100:], y_last_freqs[-100:])
-
-        # if not os.path.exists('anim'):
-        #     os.makedirs('anim')
-        # plt.savefig(os.path.join('anim', '{}.png'.format(str(j).zfill(8))), dpi=50)
+        # axes[2].plot(x_time2[-100:], y_mean_freqs[-100:])
+        axes[2].plot(x_time2[-100:], y_last_freqs[-100:])
 
         if np.all(np.isclose(phase_coherence, 1.)):
             print('\nПолная синхронизация!')
             break
 
-        # # if t_curr % (N * 10) == 0 and j != 0:
-        # if t_curr > N * 10:
-        #     prev = np.asarray([y_phase_coherence[k:k + len(phase_coherence)]
-        #                        for k in range(-N * 10, -len(phase_coherence) * 2, 1)])
-        #     corr = np.asarray([stats.pearsonr(phase_coherence, p) for p in prev])
-        #
-        #     x_time3.append(t_curr)
-        #     y_partial_stat.append([np.max(corr), np.mean(corr), np.min(corr)])
-        #     # print('max: {:.4f} \t mean: {:.4f} \t min: {:.4f}'.format(np.max(corr), np.mean(corr), np.min(corr)))
-        #     axes[3].plot(x_time3[-100:], y_partial_stat[-100:])
-        #
-        #     if np.any(np.isclose(corr, 1.)):
-        #         print('\nЧастичная синхронизация!')
-        #         break
+        f_norm = np.linalg.norm(np.asarray(y_mean_freqs[-20:]) - mean_freqs)
+        f_norms.append(f_norm)
+        f_norm2 = np.asarray(f_norms[-20:])
+        f_norm2 = np.linalg.norm(f_norm2 - np.mean(f_norm2))
+        print(f_norm, f_norm2)
+        if len(y_mean_freqs) > 20 and (f_norm < 0.1 or f_norm2 < 0.1):
+            print('\nЧастичная синхронизация!')
+            break
 
+        # plt.savefig('1/{}.png'.format(str(j).zfill(5)), dpi=300)
         plt_pause(0.00000000001)
 
     plt.show(block=True)
